e1dc75c92b12fefca348f5cbf06863ba3642ea5e
subrat001 <109800990+subrat001@users.noreply.github.com>
2022-07-30 09:39:42 +0530

    Add files via upload

---
+++ jrbc4.cpp
@@ -0,0 +1,114 @@
+/*1.Write a program to print MySirG 5 times on the screen.
+	#include<stdio.h>
+	int main()
+	{
+	int a=1;
+	while(a<=5){
+		printf("MySirG");
+	a++;
+	}
+	return 0;
+	}*/
+
+/*2. Write a program to print the first 10 natural numbers.
+	#include<stdio.h>
+	int main()
+	{
+	int a=1;
+	while(a++<=10)
+		printf("%d ",a-1);	
+	return 0;
+	}*/
+	
+/*3. Write a program to print first 10 natural numbers in reverse order.
+	#include<stdio.h>
+	int main()
+	{
+	int a=10;
+	while(a-->=1)
+		printf("%d ",a+1);
+     return 0;
+	}*/
+
+/*4. Write a program to print the first 10 odd natural numbers.
+	#include<stdio.h>
+	int main()
+	{
+	int a=1;
+		while(a<=10){
+		printf("%d ",a*2-1);
+		a++;
+		}
+    return 0;
+	}*/
+
+/*5. Write a program to print the first 10 odd natural numbers in reverse order.
+	#include<stdio.h>
+	int main()
+	{
+	int a=10;
+		while(a>=1){
+		printf("%d ",a*2-1);
+		a--;
+		}
+	return 0;
+	}*/
+/*6. Write a program to print the first 10 even natural numbers.
+	#include<stdio.h>
+	int main()
+	{
+	int a=1;
+			while(a<=10){
+		printf("%d ",a*2);
+		a++;
+		}
+	return 0;
+	}*/
+	
+/*7. Write a program to print the first 10 even natural numbers in reverse order.
+	#include<stdio.h>
+ 	int main(){
+    int a=10;
+		while(a>=1){
+		printf("%d ",a*2);
+		a--;
+		}
+	return 0; 
+ 	}*/
+ 	
+/*8. Write a program to print squares of the first 10 natural numbers.
+	#include<stdio.h>
+	int main()
+	{
+	int a=1;
+		while(a<=10){
+		printf("%d ",a*a);
+		a++;
+		}
+	return 0;
+	}*/
+	
+/*9. Write a program to print cubes of the first 10 natural numbers.
+	#include<stdio.h>
+	int main()
+	{
+	int a=1;
+		while(a<=10){
+		printf("%d ",a*a*a);
+		a++;
+		}
+	return 0;
+	}*/
+	
+/*10. Write a program to print a table of 5.
+	#include<stdio.h>
+ 	int main()
+	{
+		int a=1;
+		while(a<=10){
+		printf("5 * %d = %d\n",a,5*a);
+		a++;
+		}
+	return 0;
+	}*/
+
